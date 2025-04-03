376274f64001383df766233707b2dd92391a6852
jm0nt <4808501+jm0nt@users.noreply.github.com>
2025-04-03 13:02:25 -0700

    update setup for gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <h1 className="text-4xl font-bold text-gray-800">my site</h1>
